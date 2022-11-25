cc20cd5c246f47d1aa00184aa16d142084e90fc0
bren0r <nuriomonov1@mail.ru>
2022-11-25 12:26:24 +0300

    student expenses calculated

---
+++ ДЗ 3/04_student.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# (цикл while)
+
+# Ежемесячная стипендия студента составляет educational_grant руб., а расходы на проживание превышают стипендию
+# и составляют expenses руб. в месяц. Рост цен ежемесячно увеличивает расходы на 3%, кроме первого месяца
+# Составьте программу расчета суммы денег, которую необходимо единовременно попросить у родителей,
+# чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
+# Формат вывода:
+#   Студенту надо попросить ХХХ.ХХ рублей
+
+educational_grant, expenses = 10000, 12000
+
+expenses_calculate, expenses_total = expenses, expenses
+i = 1
+while i < 11:
+    expenses_calculate = expenses_calculate * 1.03
+    expenses_total = expenses_total + expenses_calculate
+    i += 1
+result = expenses_total - educational_grant * 11
+print('Студенту надо попросить', result, 'рублей.')
